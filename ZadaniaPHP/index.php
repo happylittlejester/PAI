ab201f407adb1b675a9fd7653841c56e2d618e1a
ladypool22 <obst.natalia@gmail.com>
2022-09-28 11:44:11 +0200

    ...

--- ZadaniaPHP/index.php
+++ ZadaniaPHP/index.php
@@ -5,6 +5,8 @@
     </head>
     <body>
         <?php
+            //tablice jednowymiarowe
+            /*
             $books[] = "Tytul1";
             $books[] = "Tytul2";
             $books[] = "Tytul3";
@@ -25,7 +27,79 @@
             echo $tab['ksiazka'];
             echo "<br>";
             echo $tab['film'];
+            */
 
+            //tablice wielowymiarowe
+            /*
+            $products = array(
+                'paper' => array(
+                    'copier' => "Do kserokopiarek i uniwersalny",
+                    'inkjet' => "Do drukarek atramentowych",
+                    'laser' => "Do drukarek laserowych",
+                    'photo' => "Papier fotograficzny"),
+                'pens' => array(
+                    'ball' => "Długopisy",
+                    'hilite' => "Markery przezroczyste",
+                    'marker' => "Markery zwykłe"),
+                'misc' => array(
+                    'tape' => "Taśmy klejące",
+                    'glue' => "Kleje",
+                    'clips' => "Spinacze")
+                );
+            
+            echo "<pre>";
+            foreach($products as $section => $items)
+                foreach($items as $key => $value)
+                    if($key == 'copier' || $key == 'ball' || $key == 'tape')
+                    {
+                        echo "$section: \t $key \t ($value)<br>";
+                    }
+            echo "</pre>";  
+            
+            /*echo $products['paper']['copier'];
+            echo "<br>";
+            echo $products['pens']['ball'];
+            echo "<br>";
+            echo $products['misc']['tape'];*/
+
+            echo "<br>";
+            
+            
+            $chessboard = array(
+                array('w','s','g','h','k','g','s','w'),
+                array('p','p','p','p','p','p','p','p'),
+                array(' ',' ',' ',' ',' ',' ',' ',' '),
+                array(' ',' ',' ',' ',' ',' ',' ',' '),
+                array(' ',' ',' ',' ',' ',' ',' ',' '),
+                array(' ',' ',' ',' ',' ',' ',' ',' '),
+                array('P','P','P','P','P','P','P','P'),
+                array('W','S','G','H','K','G','S','W'),
+            );
+            echo "<pre>";
+            foreach($chessboard as $row)
+            {
+                foreach($row as $piece)
+                {
+                    echo "$piece ";
+                }
+                echo "<br>";
+            }
+            echo "</pre>"; 
+
+            $chessboard[6][4] = ' ';
+            $chessboard[5][4] = 'P';
+
+            echo "<br>";
+            echo "<pre>";
+            foreach($chessboard as $row)
+            {
+                foreach($row as $piece)
+                {
+                    echo "$piece ";
+                }
+                echo "<br>";
+            }
+            echo "</pre>"; 
         ?>
     </body>
 </html>
