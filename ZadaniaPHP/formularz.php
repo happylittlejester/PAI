645e598ec4824a6716c27bec74e15eeb71168b4e
Natalia Obst (Uczeń) <nobst156@technikumkreatywne.pl>
2022-10-12 11:40:40 +0200

    formularze

--- ZadaniaPHP/formularz.php
+++ ZadaniaPHP/formularz.php
@@ -4,32 +4,34 @@
         <title>Formularz</title>
     </head>
     <body>
-        <form method=POST>
+        <form action="./formularz.php" method=POST>
             Login
-            <input type="text" name="login" />
+            <input type="text" name="login" required />
             Haslo
-            <input type="password" name="password" />
+            <input type="password" name="password" required />
             <p>Zapamietaj mnie</p>
             <p><input type="radio" name="remember" value="y" />Tak</p>
             <p><input type="radio" name="remember" value="n" />Nie</p>
             <input type=submit />
         </form>
         <?php
-            echo "Login: " . htmlspecialchars($_POST['login']) . ". ";
-            if(isset($_POST['remember']))
+            
+            if(isset($_POST['login']) && isset($_POST['password']) && isset($_POST['remember']))
             {
-                if($_POST[''] == 'y')
+                echo "Login: ".$_POST['login'];
+                echo "<br> Haslo: ".sha1($_POST['password']);
+                if($_POST['remember'] == 'y')
                 {
-                    echo "Wybrano opcje 'Zapamietaj mnie'";
+                    echo "<br> Wybrano opcje 'Zapamietaj mnie'";
                 }
                 else
                 {
-                    echo "Nie wybrano opcji 'Zapamietaj mnie'";
+                    echo "<br> Nie wybrano opcji 'Zapamietaj mnie'";
                 }
             }
             
         ?>
-
+        <hr>
         <form method=POST>
             Pseudonim
             <input type="text" name="pseudonim" />
@@ -38,28 +40,73 @@
             <input type=submit />
         </form>
         <?php
-        
+            if(isset($_POST['pseudonim']) && isset($_POST['textarea']))
+            {
+                echo "Pseudonim: <br>".$_POST['pseudonim'];
+                echo "<br> Komentarz 1: <br>".$_POST['textarea'];
+                echo "<br> Komentarz 2: <br>".htmlspecialchars($_POST['textarea']);
+            }
         ?>
-
+        <hr>
         <form method=POST>
             Wybierz owoc:
             <select name="owoc">
-            <option value="banan">Banan</option>
-            <option value="jablko">Jablko</option>
-            <option value="gruszka">Gruszka</option>
+            <option value="Banan">Banan</option>
+            <option value="Jablko">Jablko</option>
+            <option value="Gruszka">Gruszka</option>
             </select>
-            <p>Wybierz Zwierze:</p>
-            <p><input type="radio" name="zwierze" value="m" />Mężczyzna</p>
-            <p><input type="radio" name="zwierze" value="k" />Kobieta</p>
-            <p><input type="radio" name="plec" value="k" />Kobieta</p>
-            <p>Ulubiona seria gier:</p>
-            <p><input type="checkbox" name="gry1" value=1 />GTA</p>
-            <p><input type="checkbox" name="gry2" value=1 />FIFA</p>
-            <p><input type="checkbox" name="gry3" value=1 />Battlefield</p>
+            <p>Wybierz zwierze domowe:</p>
+            <p><input type="radio" name="zwierze" value="Pies" />Pies</p>
+            <p><input type="radio" name="zwierze" value="Kot" />Kot</p>
+            <p><input type="radio" name="zwierze" value="Ryba" />Ryba</p>
+            <p>Wybierz jezyk:</p>
+            <p><input type="checkbox" name="jezyk[]" value="Polski" />Polski</p>
+            <p><input type="checkbox" name="jezyk[]" value="Angielski" />Angielski</p>
+            <p><input type="checkbox" name="jezyk[]" value="Niemiecki" />Niemiecki</p>
             <input type=submit />
         </form>
         <?php
-        
+            if(isset($_POST['owoc']) && isset($_POST['zwierze']))
+            {
+                echo "Twoje wybory to ".$_POST['owoc']." , ".$_POST['zwierze']." , ";
+                if(isset($_POST['jezyk']))
+                {
+                    for($i = 0; $i < count($_POST['jezyk']); $i++)
+                    {
+                        echo $_POST['jezyk'][$i] . " , ";
+                    }
+                }
+                else
+                {
+                    echo "Nie wybrano ani jednego jezyka";
+                }
+            }
         ?>
+        <hr>
+        <form method = "POST">
+            Imie
+	        <input type="text" name="name" required />
+            Nazwisko
+	        <input type="text" name="lastname" required />
+	        <p>Wybierz plec:</p>
+            <p><input type="radio" name="plec" value="k" />Kobieta</p>
+            <p><input type="radio" name="plec" value="m" />Mezczyzna</p>
+            <input type="submit" />
+        </form>
+        <?php
+            if(isset($_POST['name']) && isset($_POST['lastname']) && isset($_POST['plec']))
+            {
+                if($_POST['plec'] == 'k')
+                {
+                    echo "Witam Pania ".$_POST['name'];
+                }
+                else
+                {
+                    echo "Witam Pana ".$_POST['name'];
+                }
+            }
+        ?>
+        <hr>
+        <form method = "POST">
     </body>
 </html>
